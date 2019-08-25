a08ae50eb6cdebe996635d790c931eb024b8f652
YuanDeshan <1737671795@qq.com>
2019-08-25 20:41:58 +0800

    update

--- class.cpp
+++ class.cpp
@@ -351,6 +351,7 @@ private:
 };
 #endif
 
+#if 0
 class Date
 {
 public:
@@ -374,5 +375,40 @@ int main()
 {
 	Date d;
 	d.PrintDate();
+	return 0;
+}
+#endif
+
+
+class B
+{
+public:
+	B(int b = 0)
+		:_b(b)
+	{}
+	int _b;
+};
+class A
+{
+public:
+	void Print()
+	{
+		cout << a << endl;
+		cout << b._b << endl;
+		cout << p << endl;
+	}
+private:
+	// 非静态成员变量，可以在成员声明时给缺省值。
+	int a = 10;
+	B b = 20;
+	int* p = (int*)malloc(4);
+	static int n;
+};
+int A::n = 10;
+
+int main()
+{
+	A a;
+	a.Print();
 	return 0;
 }
