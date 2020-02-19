865f1f21e5da72e1bf99ac32c7f7a9ef4ffb1e04
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-02-19 20:48:44 +0800

    Update Point_Circle.cpp

--- Point_Circle.cpp
+++ Point_Circle.cpp
@@ -3,6 +3,7 @@
 //点类
 class Point
 {
+public:
 	//设置x
 	void Setx(int x)
 	{
@@ -30,12 +31,46 @@ private:
 //圆类
 class Circle
 {
+public:
+	//设置半径
+	void Setr(int r)
+	{
+		_r = r;
+	}
+	//获取半径
+	int Getr()
+	{
+		return _r;
+	}
+	//设置圆心
+	void Setcenter(Point center)
+	{
+		_center = center;
+	}
+	//获取圆心
+	Point Getcenter()
+	{
+		return _center;
+	}
+
 private:
 	int _r;
-	Point center;
+	Point _center;
 };
+//判断点和圆的关系
+void Judge(Point &p, Circle &c)
+{
+	if()
+}
 int main()
 {
+	Point p1;
+	p1.Setx(11);
+	p1.Sety(11);
+
+	Circle c1;
+	c1.Setcenter(p1);
+	c1.Setr(10);
 
 	return 0;
 }
