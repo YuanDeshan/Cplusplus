e46b18ea8a1a2cfff4188b54bab502da8f3207c7
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-03-06 17:10:49 +0800

    Update 继承.cpp

--- 继承.cpp
+++ 继承.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#if 0
 class Basephone
 {
 public:
@@ -76,9 +77,74 @@ void test1()
 	xm.Ringup();
 	xm.xiaomiphone();
 }
+#endif
+
+#if 0
+//继承中的对象模型
+class Base
+{
+public:
+	int _a;
+protected:
+	int _b;
+private:
+	int _c;
+};
+
+class Drive:public Base
+{
+public:
+	int _d;
+};
+
+void test2()
+{
+	Drive d;
+	//父类中所有非静态成员属性都会被子类继承下去
+	//父类中私有属性 是被编译器给隐蔽了 因此是访问不到的 但还是被继承了
+	std::cout << sizeof(d) << std::endl;
+
+}
+#endif
+
+//继承中子类和父类的构造和析构顺序
+class Base
+{
+public:
+	Base()
+	{
+		std::cout << "Base构造函数" << std::endl;
+	}
+
+	~Base()
+	{
+		std::cout << "Base析构函数" << std::endl;
+	}
+};
+
+class Drive:public Base
+{
+public:
+	Drive()
+	{
+		std::cout << "Drive构造函数" << std::endl;
+	}
+
+	~Drive()
+	{
+		std::cout << "Drive析构函数" << std::endl;
+	}
+};
+
+void test3()
+{
+	Drive d;
+}
 
 int main()
 {
-	test1();
+	//test1();
+	//test2();
+	test3();
 	return 0;
 }
