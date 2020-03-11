7eb45111904e28f47d05cdced566c0a1ec39c611
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-03-11 14:00:39 +0800

    Update 饮品制作.cpp

--- 饮品制作.cpp
+++ 饮品制作.cpp
@@ -29,35 +29,63 @@ class Coffee:public AbstractDrinking
 public:
 	virtual void Boil()
 	{
-		std::cout << "烧水" << std::endl;
+		std::cout << "1.烧水" << std::endl;
 	}
 
 	virtual void ChargingFeed()
 	{
-		std::cout << "加入原料" << std::endl;
+		std::cout << "2.加入原料" << std::endl;
 	}
 
 	virtual void PourInCup()
 	{
-		std::cout << "倒入杯中" << std::endl;
+		std::cout << "3.倒入杯中" << std::endl;
 	}
 
 	virtual void AddOther()
 	{
-		std::cout << "添加辅料" << std::endl;
+		std::cout << "4.加入牛奶和白糖" << std::endl;
 	}
 
 };
 
+class Tea:public AbstractDrinking
+{
+public:
+	virtual void  Boil()
+	{
+		std::cout << "1.烧水" << std::endl;
+	}
+
+	virtual void ChargingFeed()
+	{
+		std::cout << "2.加入茶叶" << std::endl;
+	}
+
+	virtual void PourInCup()
+	{
+		std::cout << "3.倒入杯中" << std::endl;
+	}
+
+	virtual void AddOther()
+	{
+		std::cout << "4.加入柠檬" << std::endl;
+	}
+
+};
+
+//创建一个基类指针,用基类指针指向子类对象,并调用子类继承基类的函数
 void DoWork(AbstractDrinking * ad)
 {
 	ad->MakeDrinking();
-	delete ad;
+	delete ad;//一定记得销毁
 }
 
 void test01()
 {
 	DoWork(new Coffee);
+	std::cout << "=================" << std::endl;
+	DoWork(new Tea);
 }
 
 int main()
