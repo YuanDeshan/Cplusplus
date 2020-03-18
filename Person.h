dbdbdaacb362aeb22dda645892dfa750bf6268dc
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-03-18 20:39:18 +0800

    Create Person.h

---
+++ Person.h
@@ -0,0 +1,18 @@
+//#pragma once
+//#include <iostream>
+//#include <string>
+//using std::cout;
+//using std::endl;
+//using std::string;
+//
+//template <class T1,class T2>
+//class Person
+//{
+//public:
+//	Person(T1 name, T2 age);
+//	void showPerson();
+//
+//	T1 _name;
+//	T2 _age;
+//};
+//
