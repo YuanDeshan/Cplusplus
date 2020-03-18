e60d2483c5ea21380a820fd20ca86cae65ff8a12
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-03-18 20:39:24 +0800

    Create MyArray.hpp

---
+++ MyArray.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream> 
+using std::cout;
+using std::endl;
+using std::string;
+
+template <class T>
+class MyArray
+{
+public:
+
+private:
+	T* pArray;
+	int _capacity;
+	int _size;
+
+};
