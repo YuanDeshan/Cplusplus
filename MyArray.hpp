a54c461b75c864d5b83947dce3fff508c3bd7ff0
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-03-19 19:23:05 +0800

    Update MyArray.hpp

--- MyArray.hpp
+++ MyArray.hpp
@@ -8,9 +8,24 @@ template <class T>
 class MyArray
 {
 public:
+	MyArray(int capacity)
+	{
+		this->_capacity = capacity;
+		this->_size = 0;
+		this->pArray = new T[_capacity];
+	}
 
+
+	~MyArray()
+	{
+		if (this->pArray != NULL)
+		{
+			delete[] this->pArray;
+			this->pArray = NULL;
+		}
+	}
 private:
-	T* pArray;
+	T* pArray;  //指针指向堆区开辟的真实数组
 	int _capacity;
 	int _size;
 
