02173b2db29b6f62b1fad7f7a640d72935f21826
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-03-23 21:57:15 +0800

    Update VectorP.cpp

--- VectorP.cpp
+++ VectorP.cpp
@@ -100,11 +100,45 @@ void test03()
 
 }
 
+//ÈÝÆ÷Ç¶Ì×ÈÝÆ÷
+void test04()
+{
+	vector<vector<int>> v;
+	vector<int> v1;
+	vector<int> v2;
+	vector<int> v3;
+	vector<int> v4;
+
+	for (int i=0; i < 4; ++i)
+	{
+		v1.push_back(i);
+		v2.push_back(i+1);
+		v3.push_back(i+2);
+		v4.push_back(i+3);
+	}
+
+	v.push_back(v1);
+	v.push_back(v2);
+	v.push_back(v3);
+	v.push_back(v4);
+
+	for (vector<vector<int>>::iterator it = v.begin(); it != v.end(); ++it)
+	{
+		//(*it) Îªvector<int>
+		for (vector<int>::iterator vit = (*it).begin(); vit != (*it).end(); ++vit)
+		{
+			cout << *vit << " ";
+		}
+		cout << endl;
+	}
+}
+
 
 int main()
 {
 	//test01();
 	//test02();
-	test03();
+	//test03();
+	test04();
 	return 0;
 }
