b908affd56655032bf138021c70ed3b343e4a91d
YuanDeshan <48439253+YuanDeshan@users.noreply.github.com>
2020-04-02 16:47:49 +0800

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -38,8 +38,54 @@ void test01()
 	PrintList(L4);
 }
 
+//list赋值
+void test02()
+{
+	list<int> L1;
+	L1.push_back(1);
+	L1.push_back(2);
+	L1.push_back(3);
+	L1.push_back(4);
+	L1.push_back(5);
+	PrintList(L1);
+
+	list<int> L2;
+	L2.assign(L1.begin(), L1.end());
+	PrintList(L2);
+
+	list<int> L3;
+	L3 = L2;
+	PrintList(L3);
+
+	list<int> L4;
+	L4.assign(5, 999);
+	PrintList(L4);
+}
+
+//list赋值
+void test03()
+{
+	list<int> L1;
+	L1.push_back(1);
+	L1.push_back(2);
+	L1.push_back(3);
+	L1.push_back(4);
+	L1.push_back(5);
+
+	list<int> L2(5,999);
+	cout << "交换前: " << endl;
+	PrintList(L1);
+	PrintList(L2);
+
+	cout << "交换后: " << endl;
+	PrintList(L1);
+	PrintList(L2);
+}
+
 int main()
 {
-	test01();
+	//test01();
+	//test02();
+	test03();
 	return 0;
 }
